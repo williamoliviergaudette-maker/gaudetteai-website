ea5f9a447ae3f2dea8054e24aa625891e317b08e
William-Olivier Gaudette <william.olivier.gaudette@gmail.com>
2026-01-26 23:53:07 -0500

    Add construction page for maintenance periods

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://gaudetteai.com',
-  integrations: [tailwind(), sitemap()],
-});
